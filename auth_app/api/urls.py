d8264594a72b8478de43ac5e77178e8dd63ee95d
AlxRpp <alexanderr26@outlook.de>
2026-06-16 16:12:19 +0200

    added login endpoint

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegistrationView
+from .views import RegistrationView, LoginUserView
 
 urlpatterns = [
-    path('registration/', RegistrationView.as_view(), name='registration')
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', LoginUserView.as_view(), name='login')
+
 ]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -4,6 +4,8 @@ from .serializers import RegistrationSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class RegistrationView(APIView):
@@ -28,3 +30,33 @@ class RegistrationView(APIView):
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(data, status=status.HTTP_201_CREATED)
+
+
+class LoginUserView(APIView):
+    permission_classes = [AllowAny]
+    data = {}
+
+    def post(self, request):
+        try:
+            email = request.data.get('email')
+            password = request.data.get('password')
+            if not email or not password:
+                return Response({'ErrorMessage': 'Please enter valid credentials '}, status=status.HTTP_400_BAD_REQUEST)
+
+            user = User.objects.get(email=email)
+            pw_check = user.check_password(password)
+
+            if pw_check:
+                token, created = Token.objects.get_or_create(user=user)
+                self.data = {
+                    'token': token.key,
+                    'username': user.username,
+                    'email': user.email,
+                    'user_id': user.id
+                }
+            else:
+                return Response({'ErrorMessage': 'Please enter valid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({'ErrorMessage': 'Please enter valid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(self.data, status=status.HTTP_200_OK)
